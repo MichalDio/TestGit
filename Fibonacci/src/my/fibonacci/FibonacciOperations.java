708ba9873b910deb53d4c2f22a1d02c57c1bc77c
Michal Dio <Michal.Dio@4finance.com>
2017-06-09 14:02:42 +0200

    initial version

---
+++ Fibonacci/src/my/fibonacci/FibonacciOperations.java
@@ -0,0 +1,11 @@
+package my.fibonacci;
+
+/**
+ * Created by michal.dio on 02/06/2017.
+ */
+public interface FibonacciOperations {
+
+    public int getFibonacci(int n);
+
+    public int sumFibonacci(int n);
+}
