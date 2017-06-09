3d61ea7b200207dc2eac734d3482ea1ed1ab1835
Michal Dio <Michal.Dio@4finance.com>
2017-06-09 14:04:26 +0200

    initial version

--- Fibonacci/src/my/fibonacci/FibbonacciApp.java
+++ Fibonacci/src/my/fibonacci/FibbonacciApp.java
@@ -4,4 +4,11 @@ package my.fibonacci;
  * Created by michal.dio on 05/05/2017.
  */
 public class FibbonacciApp {
+
+    public static void main(String[] args) {
+        System.out.println("Running fibonacci ...");
+        FibonacciOperations calc = new FibonacciRecursionCalculator();
+        int sum = calc.sumFibonacci(5);
+        System.out.println("Sum:"+sum);
+    }
 }
