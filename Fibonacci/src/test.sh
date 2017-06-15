ae042c6a7c65958ecc1a731079acb4745eda592a
Michal Dio <Michal.Dio@4finance.com>
2017-06-15 16:25:11 +0200

    initial version

---
+++ Fibonacci/src/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+#cd  C:/Users/michal.dio/IdeaProjects/TestGit/out/production/Fibonacci/my/fibonacci
+
+ls
+
+
+java -classpath C:/Users/michal.dio/IdeaProjects/TestGit/out/production/Fibonacci my.fibonacci.FibonacciApp
+
